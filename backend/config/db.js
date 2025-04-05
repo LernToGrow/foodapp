b926c06422e8eef4396398b932edc2d45727b068
LernToGrow <71166331+LernToGrow@users.noreply.github.com>
2025-04-05 10:33:18 +0530

    Update db.js new url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://greatstack:999999999@cluster0.epeuz.mongodb.net/food-del").then(()=>{console.log("DB Conected");
-    }) 
-}
+    // Set the strictQuery option before connecting to MongoDB
+    mongoose.set('strictQuery', true);
+
+    try {
+        // Attempt to connect to MongoDB
+        await mongoose.connect("mongodb+srv://sumitkamble1252:jdBVCo4PoX925K3u@fatomato.bs9r3l2.mongodb.net/?retryWrites=true&w=majority&appName=FATomato");
+        console.log("DB Connected");
+    } catch (err) {
+        console.error("Error connecting to DB:", err);
+    }
+};
